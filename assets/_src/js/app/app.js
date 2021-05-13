88457dd27182a0e0f3a9c99be068a97bfa9f62b1
Ian <ian.wiedenman@gmail.com>
2021-05-13 00:34:06 -0700

    Init 🚀

---
+++ assets/_src/js/app/app.js
@@ -0,0 +1,14 @@
+window.$ = require('jquery');
+window.jQuery = window.$;
+require('jquery-migrate');
+require('bootstrap');
+
+require('../theme/theme.min.js');
+
+Manager.ready(function() {
+  Manager.log('app.js fired Manager.ready()');
+
+  // Add additional logic here!
+  // var theme = require('../theme/theme.js');
+
+});
