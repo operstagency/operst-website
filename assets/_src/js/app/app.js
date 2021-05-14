fd62bca9a83edc346e25179e92d87dcf20df8a60
Ian <ian.wiedenman@gmail.com>
2021-05-13 18:17:36 -0700

    Added services page and about page, finished homepage.

--- assets/_src/js/app/app.js
+++ assets/_src/js/app/app.js
@@ -10,5 +10,20 @@ Manager.ready(function() {
 
   // Add additional logic here!
   // var theme = require('../theme/theme.js');
+  // var currentUrl = window.location.href;
+  if (window.location.pathname === '/') {
+    import('../../../_src-uncompiled/vendor/slick-carousel/slick/slick.js')
+      .then(function(mod) {
+        import('../../../_src-uncompiled/js/hs.slick-carousel.js')
+          .then(function(mod) {
+            $('.js-slick-carousel').each(function() {
+              console.log('---this', this);
+              var slickCarousel = $.HSCore.components.HSSlickCarousel.init($(this));
+            });
+          })
+      })
+
+
+  }
 
 });
