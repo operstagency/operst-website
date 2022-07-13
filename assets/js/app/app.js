c4ba65b57f0748ab367e352b1c7a94b2844c68e4
iwiedenm <iwiedenm@users.noreply.github.com>
2022-07-13 12:27:02 +0000

    jekyll build from Action 4595621d17d9041ab0c0609d31473e6564c28077

---
+++ assets/js/app/app.js
@@ -0,0 +1,26 @@
+window.$ = require('jquery');
+window.jQuery = window.$;
+require('jquery-migrate');
+require('bootstrap');
+
+require('../theme/theme.min.js');
+
+Manager.ready(function() {
+  Manager.log('app.js fired Manager.ready()');
+
+  // Add additional logic here!
+  // var theme = require('../theme/theme.js');
+  // var currentUrl = window.location.href;
+  if (window.location.pathname === '/') {
+    import('../../../_src-uncompiled/vendor/slick-carousel/slick/slick.js')
+      .then(function(mod) {
+        import('../../../_src-uncompiled/js/hs.slick-carousel.js')
+          .then(function(mod) {
+            $('.js-slick-carousel').each(function() {
+              var slickCarousel = $.HSCore.components.HSSlickCarousel.init($(this));
+            });
+          })
+      })
+  }
+
+});
